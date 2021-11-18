d2019bfe70c554f2e730c0c0fa582145dd99f6a1
Nicolas <nicolas@infolinesystems.com.br>
2021-11-17 23:48:29 -0300

    aa

--- src/main/java/org/hathor/Hathor/HathorApplication.java
+++ src/main/java/org/hathor/Hathor/HathorApplication.java
@@ -3,14 +3,8 @@ package org.hathor.Hathor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//@EnableJpaRepositories("org.hathor.Hathor.repository")
-//@EntityScan("org.hathor.Hathor.model")
-//@ComponentScan("org.hathor.Hathor.service")
-//@EnableWebSecurity
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @SpringBootApplication()
-public class HathorApplication {
+public class HathorApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(HathorApplication.class, args);
